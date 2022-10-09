588f9f1df6f1f8d64766ce1a0a0a278b2487405a
Joni Turunen <joni.turunen@triplan.fi>
2022-10-09 23:10:13 +0300

    Add initial commit for part1

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,9 +1,50 @@
 package main
 
 import (
+	"encoding/csv"
 	"log"
+	"os"
+	"strings"
 )
 
+type Weather struct {
+	dayNumber int
+	maxTemp   int
+	minTemp   int
+}
+
 func main() {
-	log.Println("Im waiting to be implemented")
-} 
+	sliceOfSliceOfStr := readFile()
+
+	for _, record := range sliceOfSliceOfStr {
+		newStr := strings.Join(strings.Fields(record[0]), " ")
+		splittedStr := strings.Split(newStr, " ")
+		log.Printf("Splitted string is: %T\n", splittedStr)
+		// TODO: convert splittedStr to int values
+		// TODO: create a Weather struct with the values
+		// TODO: find the day with the smallest temperature spread
+	}
+}
+
+func readFile() [][]string {
+	file, err := os.Open("weather.dat")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// strings.Join(strings.Fields(file), " ")
+
+	log.Printf("File type is: %T", file)
+
+	reader := csv.NewReader(file)
+	reader.Comma = '\t'
+	reader.FieldsPerRecord = -1
+
+	rawData, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return rawData
+}
