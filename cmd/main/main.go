129b5982950b9b65b5d14906b024f4da55a7822c
Joni Turunen <joni.turunen@triplan.fi>
2022-10-15 21:19:46 +0300

    Move file read to separate file

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/csv"
 	"log"
-	"os"
 	"regexp"
 	"strings"
 )
@@ -59,30 +57,4 @@ func removeNonNumericalChars(s string) string {
 		return reg.ReplaceAllString(s, "")
 	}
 	return s
-}
-
-func readFile() [][]string {
-	file, err := os.Open("weather.dat")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer closeFile(file)
-
-	reader := csv.NewReader(file)
-	reader.Comma = '\t'
-	reader.FieldsPerRecord = -1
-
-	rawData, err := reader.ReadAll()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return rawData
-}
-
-func closeFile(f *os.File) {
-	err := f.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
 }
